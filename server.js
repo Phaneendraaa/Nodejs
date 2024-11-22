60cff4a20021fb7a9380a5e50efd09249dcafc1b
naga12334 <154617811+naga12334@users.noreply.github.com>
2024-11-22 19:05:49 +0530

    Notes project without Database

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const app = express();
+const fs = require("fs");
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.set("view engine","ejs"); //ejs is almost similar to html but addssome dynamic functionality
-app.use(express.static(path.join(__dirname,"public")));  //given static files location as public
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended:true}));
+app.set("view engine","ejs");
+
 app.get("/",function(req,res){
-    res.render("index");
+    fs.readdir("./files",function(err,files){
+        res.render("index",{files:files});
+    });
+    
 });
-app.get("/:username",function(req,res){
-    res.send(`username is ${req.params.username}`)
+app.post("/formcatch",function(req,res){
+        fs.writeFile(`./files/${(req.body.title).split(" ").join("")}.txt`,`${req.body.btn}`,function(err){})
+        res.redirect("/");
 });
-app.get("/:username/:age",function(req,res){
-    res.send(`username is ${req.params.username} and age is ${req.params.age}`)
+app.get("/details/:val", function(req, res) {
+    const filename = "./files/"+req.params.val;
+    fs.readFile(filename, function(err, filedata) {
+        
+        res.render("detail", { filedata: filedata });
+    });
 });
+app.get("/edit/:filenam", function(req, res) {
+    const x = req.params.filenam;
+    res.render("editpage",{previousName:x})
+});
+app.post("/edit",function(req,res){
+    const PrevName = path.join(__dirname, "files", req.body.Prev);
+    const newName = path.join(__dirname, "files", req.body.New);
+
+    fs.rename(PrevName,newName+".txt",function(err){
+        res.redirect("/");
+    })
+})
 app.listen(3000);
